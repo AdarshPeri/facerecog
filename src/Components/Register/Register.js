a4bc0cf8ff1f94487cdaa6c7bb19148f048bb3d2
adarshperi <adarshperi@gmail.com>
2019-06-18 06:14:03 +0530

    silly

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     	this.setState({password: event.target.value})
     }
     onSubmitSignIn = () => {
-    	fetch('http://localhost:3001/register',{
+    	fetch('https://secret-sea-63371.herokuapp.com/register',{
     		method: 'post',
     		headers: {'Content-Type':'application/json'},
     		body: JSON.stringify({
